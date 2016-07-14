db4acd77de0ebd8e9af99d5e919869340606ef1b
Soufyan Belkaid <soufyanbelkaid@dhcp-014-245.public.vu.nl>
2016-07-14 15:02:38 +0200

    MAP evaluation

---
+++ evaluation.py
@@ -0,0 +1,34 @@
+import sys
+import os
+import pandas as pd
+from average_precision import *
+
+
+usage = '''
+usage: python evaluation.py <path_to_file>
+'''
+
+file_name = sys.argv[1]
+if not os.path.isfile(file_name):
+	print usage
+	sys.exit()
+
+result_file = open(file_name, 'r')
+
+df = pd.read_csv(result_file)
+
+seq = df[df.columns.values[4:]]
+l = [df[df[col].notnull()][col].values for col in df.columns[4:]]
+
+real = []
+predicted = []
+for idx, val in enumerate(l):
+	if idx % 2 == 0:
+		print 'added to predicted: ', idx
+		#doesn't work with numpy.array
+		predicted.append(val.tolist())
+	else:
+		real.append(val.tolist())
+
+print mapk(real, predicted)
+
