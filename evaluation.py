b4297ef4cd6f099baab7dc911990d87ec6373b18
Soufyan Belkaid <soufyanbelkaid@MacBook-Pro-4.local>
2016-07-15 13:41:25 +0200

    eval

--- evaluation.py
+++ evaluation.py
@@ -5,10 +5,16 @@ from average_precision import *
 
 
 usage = """
-usage: python evaluation.py <path_to_file>
+usage: python evaluation.py <path_to_file> <precision_at_k>
 """
 
 file_name = sys.argv[1]
+if len(sys.argv) < 3:
+	print 'forgot a parameter'
+	print usage
+	sys.exit(1)
+
+k = int(sys.argv[2])
 if not os.path.isfile(file_name):
 	print usage
 	sys.exit(1)
@@ -17,18 +23,17 @@ result_file = open(file_name, 'r')
 
 df = pd.read_csv(result_file)
 
-seq = df[df.columns.values[4:]]
-l = [df[df[col].notnull()][col].values for col in df.columns[4:]]
+seq = df[df.columns.values]
+data = [df[df[col].notnull()][col].values for col in df.columns]
 
 real = []
 predicted = []
-for idx, val in enumerate(l):
+for idx, val in enumerate(data):
 	if idx % 2 == 0:
 		print 'added to predicted: ', idx
 		#doesn't work with numpy.array
-		predicted.append(val.tolist())
+		predicted.append([e.lower().strip(' ') for e in val.tolist()])
 	else:
-		real.append(val.tolist())
+		real.append([e.lower().strip(' ') for e in val.tolist()])
 
-print 'MAP val is: {}'.format(mapk(real, predicted))
-
+print 'MAP val is: {}'.format(mapk(real, predicted, k))
